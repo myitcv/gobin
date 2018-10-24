7839f458c4f1fe38b8f555cb4de6a6bde7487653
Paul Jolly <paul@myitcv.io>
2018-10-24 01:12:14 +0100

    Initial cut of gobin (#1)

---
+++ .readmescript.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# **START**
+
+echo "machine github.com login $GITHUB_USERNAME password $GITHUB_PAT" >> $HOME/.netrc
+echo "" >> $HOME/.netrc
+echo "machine api.github.com login $GITHUB_USERNAME password $GITHUB_PAT" >> $HOME/.netrc
+git config --global user.email "$GITHUB_USERNAME@example.com"
+git config --global user.name "$GITHUB_USERNAME"
+git config --global advice.detachedHead false
+git config --global push.default current
+
+# block: clone
+export GO111MODULE=on
+git clone --branch initial_cut https://github.com/myitcv/gobin /tmp/gobin
+cd /tmp/gobin
+
+# block: install
+go install
+
+# block: fix path
+export PATH=$(go env GOPATH)/bin:$PATH
+which gobin
+
+# block: use
+gobin -r github.com/rogpeppe/gohack@master -help
+assert "$? -eq 2" $LINENO
---
+++ script_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/rogpeppe/go-internal/goproxytest"
+	"github.com/rogpeppe/go-internal/gotooltest"
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+var proxyURL string
+
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(gobinMain{m}, map[string]func() int{
+		"gobin": mainint,
+	}))
+}
+
+type gobinMain struct {
+	m *testing.M
+}
+
+func (m gobinMain) Run() int {
+	if os.Getenv("GO_GCFLAGS") != "" {
+		fmt.Fprintf(os.Stderr, "testing: warning: no tests to run\n") // magic string for cmd/go
+		fmt.Printf("cmd/go test is not compatible with $GO_GCFLAGS being set\n")
+		fmt.Printf("SKIP\n")
+		return 0
+	}
+	os.Unsetenv("GOROOT_FINAL")
+
+	// Start the Go proxy server running for all tests.
+	srv, err := goproxytest.NewServer("testdata/mod", "")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot start proxy: %v", err)
+		return 1
+	}
+	proxyURL = srv.URL
+
+	return m.m.Run()
+}
+
+func TestScripts(t *testing.T) {
+	p := testscript.Params{
+		Dir: "testdata",
+		Setup: func(e *testscript.Env) error {
+			e.Vars = append(e.Vars, "GOPROXY="+proxyURL)
+			return nil
+		},
+	}
+	if err := gotooltest.Setup(&p); err != nil {
+		t.Fatal(err)
+	}
+	testscript.Run(t, p)
+}
